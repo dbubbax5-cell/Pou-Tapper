5a4cb3bfd91da5bced803caa7e9c97b23f60b376
dbubbax5-cell <dbubbax5@gmail.com>
2026-02-25 19:37:48 +1100

    dih

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 // unleash your creativity and write your code here.
 
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx"
+import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
