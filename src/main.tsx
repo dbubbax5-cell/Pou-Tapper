2bb4d82ccdf2c456b48b33d2d713ef8f748fa542
dbubbax5-cell <dbubbax5@gmail.com>
2026-02-25 19:43:40 +1100

    dih bee

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-// unleash your creativity and write your code here.
-
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <App />
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  createRoot(rootElement).render(<App />);
+}
