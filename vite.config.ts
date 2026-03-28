f5908d1f12efa47ca75c5105eef5a034ae565b71
dbubbax5-cell <dbubbax5@gmail.com>
2026-03-28 15:34:45 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  , base: '/Pou-Tapper/',
+ base: '/Pou-Tapper/',
  plugins:[react()]// other configurations
 });
